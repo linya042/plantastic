02bd502e319da518465306e7d140662aabbfa0ae
Linya <yan.kuimov2014@gmail.com>
2025-04-30 16:01:28 +0500

    push

--- bot.py
+++
@@ -1,7 +0,0 @@
-import telebot
-from telebot import types
-bot = telebot.TeleBot('7939672090:AAFII-LokIhrpPUObg15PLjq8xjDmgOT0GE')
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, "Привет Садовод держи ссылку<3")
-bot.infinity_polling()
